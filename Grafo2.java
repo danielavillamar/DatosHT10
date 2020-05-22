cda96aa674d494c7f1b0340cd6098ce8f09b5537
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-05-21 19:17:47 -0600

    Create Grafo2.java

---
+++ Grafo2.java
@@ -0,0 +1,12 @@
+public class Grafo2 {
+	
+	int startVertex;
+    int endVertex;
+    int weight;
+
+    public Grafo2 (int start, int end, int distance){
+        startVertex = start;
+        endVertex = end;
+        weight = distance;
+    }
+}
